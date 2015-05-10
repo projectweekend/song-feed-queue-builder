b4beab88c3563f6910bdc623acda311486b17501
Brian Hines <brian@projectweekend.net>
2015-05-10 15:11:19 -0500

    Drinking the AWS kool-aid

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
-import os
+import json
 from datetime import date
-from pymongo import MongoClient
-from pika_pack import Sender
+from boto import sqs
+from boto.dynamodb2.table import Table
 
 
-DOCKER_MONGO_IP_PORT = '{0}:{1}'.format(
-    os.getenv('DB_PORT_27017_TCP_ADDR'),
-    os.getenv('DB_PORT_27017_TCP_PORT'))
-MONGO_URL = os.getenv('MONGO_URL', 'mongodb://{0}/test'.format(DOCKER_MONGO_IP_PORT))
-DOCKER_RABBIT_IP_PORT = '{0}:{1}'.format(
-    os.getenv('RABBIT_PORT_5672_TCP_ADDR'),
-    os.getenv('RABBIT_PORT_5672_TCP_PORT'))
-RABBIT_URL = os.getenv('RABBIT_URL', 'amqp://{0}/'.format(DOCKER_RABBIT_IP_PORT))
+def playlists_to_process():
+    accounts = Table('accounts')
+    target_date = date.isoformat(date.today())
+    return accounts.scan(last_processed__ne=target_date)
 
 
-def mongo_connect():
-    client = MongoClient(MONGO_URL)
-    return client.get_default_database()
+def playlists_queue():
+    conn = sqs.connect_to_region('us-east-1')
+    return conn.create_queue('song-feed-playlists-to-process')
 
 
 def main():
-    sender = Sender(RABBIT_URL, 'song-feed')
-    db = mongo_connect(MONGO_URL)
-    query = {
-        'last_processed': {
-            '$ne': date.isoformat(date.today())
-        }
-    }
-    playlists = db.playlists.find(query)
-    for playlist in playlists:
-        sender.send('song-feed', playlist)
+    q = playlists_queue()
+    for playlist in playlists_to_process():
+        body = json.dumps(dict(playlist.items()))
+        print(body)
+        q.write(q.new_message(body=body))
 
 
 if __name__ == '__main__':
